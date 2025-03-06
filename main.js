aff4340b145067871e2d32d539965492cd23b085
gpt183 <tejabondu@gmail.com>
2025-03-06 11:01:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var user=document.querySelector("#user");
+    var pass=document.querySelector("#pass");
+    var btn1=document.querySelector("#btn1");
+    btn1.addEventListener("click",()=>{
+        if(!(user.value=="gpgnvm" && pass.value="gpt@183")){
+            btn1.disabled="true";
+        }
+    });
     const container = document.querySelector('.container');
     const registerBtn = document.querySelector('.register-btn');
     const loginBtn = document.querySelector('.login-btn');
